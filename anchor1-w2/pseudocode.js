50c4f64daf247d28cbac261be53b8ffd16790020
unknown <rahmatgilangmaulana9@gmail.com>
2020-03-24 13:13:51 +0700

    week-2 hacktiv8 initial commit

---
+++ anchor1-w2/pseudocode.js
@@ -0,0 +1,45 @@
+//Algorithm and Pseudocode
+
+//1. ---------Algorithm---------- RESULTAN GAYA
+//  - m adalah nilai massa benda
+//  - a adalah nilai percepatan
+//  - ΣF adalah massa benda x percepatan
+//  ---------Pseudocode-----------
+//  - SET m = 600
+//  - SET a = 2
+//  - SET m*a = ΣF
+//  - DISPLAY ΣF
+
+//2. ---------Algorithm---------- TAHUN KABISAT
+//  - x adalah angka tahun yang di tes
+//  - jika x habis di bagi 4 maka tahun itu tersebut kabisat
+//  - jika x habis dibagi 100 dan habis dibagi 400 maka tahun tersebut kabisat
+//  - jika x habis dibagi 4 dan tidak habis dibagi 100 maka tahun tersebut kabisat
+//  ---------Pseudocode-----------
+//  - SET tahun = x
+//  - IF x modulus 400 = 0
+//  - DISPLAY tahun x adalah tahun kabisat
+//  - ELSE IF x modulus 100 = 0
+//  - DISPLAY x adalah tahun kabisat
+//  - IF ELSE x modulus 4 = 0
+//  - DISPLAY x adalah tahun kabisat
+//  - ELSE DISPLAY tahun x bukan tahun kabisat
+
+//3. ---------Algorithm---------- LAUNDRY DAY
+//  - mesin baru dinyalakan setelah ada 20 baju di dalamnya
+//  ---------Pseudocode-----------
+//  - SET baju = i
+//  - FOR ( SET i = 1, i < 21, i INCREMENT COUNTER)
+//  - DISPLAY i
+
+//4. ---------Algorithm---------- PERIKSA KUKU
+//  - jumlah murid 40
+//  - jika kuku siswa/siswi panjang siswa/siswi dihukum
+//  - jika kuku siswa/siswi tidak panjang siswa/siswi mendapat pujian
+//  ---------Pseudocode-----------
+//  - SET kuku = x
+//  - IF x = PANJANG
+//  - DISPLAY SISWA MENDAPAT HUKUMAN
+//  - ELSE IF x = PENDEK
+//  - DISPLAY SISWA MENDAPAT PUJIAN
+//  - ELSE DISPLAY KATEGORI KUKU HANYA PANJANG / PENDEK
