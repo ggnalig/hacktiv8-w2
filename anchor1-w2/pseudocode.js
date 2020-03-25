8a92aace5c38253d6697c837c2494f048765e48e
unknown <rahmatgilangmaulana9@gmail.com>
2020-03-25 21:05:01 +0700

    perbaikan = ganti equals to, end for & end if

--- anchor1-w2/pseudocode.js
+++ anchor1-w2/pseudocode.js
@@ -5,9 +5,9 @@
 //  - a adalah nilai percepatan
 //  - ΣF adalah massa benda x percepatan
 //  ---------Pseudocode-----------
-//  - SET m = 600
-//  - SET a = 2
-//  - SET m*a = ΣF
+//  - SET m EQUALS TO 600
+//  - SET a EQUALS TO 2
+//  - SET m*a EQUALS TO ΣF
 //  - DISPLAY ΣF
 
 //2. ---------Algorithm---------- TAHUN KABISAT
@@ -16,30 +16,33 @@
 //  - jika x habis dibagi 100 dan habis dibagi 400 maka tahun tersebut kabisat
 //  - jika x habis dibagi 4 dan tidak habis dibagi 100 maka tahun tersebut kabisat
 //  ---------Pseudocode-----------
-//  - SET tahun = x
-//  - IF x modulus 400 = 0
+//  - SET tahun EQUALS TO x
+//  - IF x modulus 400 EQUALS TO 0
 //  - DISPLAY tahun x adalah tahun kabisat
-//  - ELSE IF x modulus 100 = 0
+//  - ELSE IF x modulus 100 EQUALS TO 0
 //  - DISPLAY x adalah tahun kabisat
-//  - IF ELSE x modulus 4 = 0
+//  - IF ELSE x modulus 4 EQUALS TO 0
 //  - DISPLAY x adalah tahun kabisat
 //  - ELSE DISPLAY tahun x bukan tahun kabisat
+//  - END IF
 
 //3. ---------Algorithm---------- LAUNDRY DAY
 //  - mesin baru dinyalakan setelah ada 20 baju di dalamnya
 //  ---------Pseudocode-----------
-//  - SET baju = i
-//  - FOR ( SET i = 1, i < 21, i INCREMENT COUNTER)
+//  - SET baju EQUALS TO i
+//  - FOR ( SET i EQUALS TO 1, i < 21, i INCREMENT COUNTER)
 //  - DISPLAY i
+//  - END FOR
 
 //4. ---------Algorithm---------- PERIKSA KUKU
 //  - jumlah murid 40
 //  - jika kuku siswa/siswi panjang siswa/siswi dihukum
 //  - jika kuku siswa/siswi tidak panjang siswa/siswi mendapat pujian
 //  ---------Pseudocode-----------
-//  - SET kuku = x
-//  - IF x = PANJANG
+//  - SET kuku EQUALS TO x
+//  - IF x EQUALS TO PANJANG
 //  - DISPLAY SISWA MENDAPAT HUKUMAN
-//  - ELSE IF x = PENDEK
+//  - ELSE IF x EQUALS TO PENDEK
 //  - DISPLAY SISWA MENDAPAT PUJIAN
 //  - ELSE DISPLAY KATEGORI KUKU HANYA PANJANG / PENDEK
+//  - END IF
