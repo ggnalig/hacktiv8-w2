111ed34bddfcb3da0b1dad437824341e022af5b5
unknown <rahmatgilangmaulana9@gmail.com>
2020-03-27 14:38:54 +0700

    initial commit

---
+++ anchor8,9-w2/main-function.js
@@ -0,0 +1,31 @@
+//TUGAS 1
+console.log('Tugas 1');
+function shoutOut() {
+    return('Halo Function!');
+}
+console.log(shoutOut());
+
+//TUGAS 2
+console.log('Tugas 2');
+function calculateMultiply(num1,num2) {
+    return num1 * num2;    
+}
+
+var num1 = 6;
+var num2 = 5;
+var hasilPerkalian = calculateMultiply(num1, num2);
+console.log(hasilPerkalian);
+
+//TUGAS 3
+console.log('Tugas 3');
+function processSentence(name,age,address,hobby) {
+    return(`Halo nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}!`)
+}
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence = processSentence(name,age,address,hobby);
+console.log(fullSentence);
